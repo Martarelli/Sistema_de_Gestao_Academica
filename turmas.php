35e5b09e11bef6b65c26bb003680bb0b4ad717b5
Martarelli <renanmartarelli@gmail.com>
2023-06-04 17:52:53 -0300

    listagem de turmas

--- turmas.php
+++ turmas.php
@@ -11,40 +11,40 @@ require("sidemenu.php");
 require_once('connection.php');
 
 // Mysql query to select data from table
-$mysql_query = "SELECT * FROM turmas ORDER BY id";
+$mysql_query = "SELECT * FROM turma ORDER BY codigo";
 $result = $conn->query($mysql_query);
 
 //Connection Close
 mysqli_close($conn);
 ?> 
-<div class="container">
+<div class="container p-3">
   <h2>Turmas</h2>
   <p>Listagem das turmas cadastradas.</p>
   <hr>
   <div class="float-right p-1">
-    <a href="insert-turma.php"><button type="button" class="btn btn-primary">Adicionar Turma</button></a>
+    <a href="insert-turma.php"><button type="button" class="btn btn-dark mb-3">Adicionar Turma</button></a>
   </div>
   <table class="table table-striped table-bordered table-hover">
     <thead>
       <tr class="table-info" style="text-align:center">
-        <th scope="col" style="width: 5%;">#</th>
-        <th scope="col">Nome</th>
-        <th scope="col" style="width: 30%;">E-Mail</th>
-        <th scope="col" style="width: 15%;">Data Nascimento</th>
+        <th scope="col" style="width: 10%;">Código</th>
+        <th scope="col">Curso</th>
+        <th scope="col" style="width: 15%;">Semestre</th>
+        <th scope="col" style="width: 15%;">Periodo</th>
         <th scope="col" style="width: 20%;">Ação</th>
       </tr>
     </thead>
     <tbody>
       <?php while($data = mysqli_fetch_array($result)) { ?> 
       <tr> 
-        <th scope="row" style="text-align:center"><?php echo $data['id']; ?></th>
-        <td><?php echo $data['nome']; ?></td> 
-        <td><?php echo $data['email']; ?></td> 
-        <td style="text-align:center"><?php echo date('d/m/Y', strtotime($data['datanasc'])); ?></td>
+        <th scope="row" style="text-align:center"><?php echo $data['codigo']; ?></th>
+        <td style="text-align:center"><?php echo $data['curso']; ?></td> 
+        <td style="text-align:center"><?php echo $data['semestre']; ?></td> 
+        <td style="text-align:center"><?php echo $data['periodo']; ?></td> 
         <td style="text-align:center">
-          <a href="update-contato.php?id=<?php echo $data['id']; ?>">
+          <a href="update-turma.php?id=<?php echo $data['codigo']; ?>">
             <button type="button" class="btn btn-primary">Editar</button></a>
-          <a href="delete-contato.php?id=<?php echo $data['id']; ?>">
+          <a href="delete-turma.php?id=<?php echo $data['codigo']; ?>">
             <button type="button" class="btn btn-danger">Excluir</button></a>
         </td> 
       </tr> 
@@ -52,5 +52,3 @@ mysqli_close($conn);
     </tbody>
   </table>
 </div>
-
-<?php require("footer-inc.php"); ?>
