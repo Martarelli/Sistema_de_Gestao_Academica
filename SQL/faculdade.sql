ab7cb02dec9bdce777847205890c7f1286da6f22
Martarelli <renanmartarelli@gmail.com>
2023-06-04 18:27:00 -0300

    alteracao table disciplina e prof

--- SQL/faculdade.sql
+++ SQL/faculdade.sql
@@ -32,31 +32,32 @@ CREATE TABLE IF NOT EXISTS `faculdade`.`aluno` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 
-DROP TABLE IF EXISTS `faculdade`.`disciplina` ;
-CREATE TABLE IF NOT EXISTS `faculdade`.`disciplina` (
-  `codigo` INT(11) NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(45) NOT NULL,
-  `descricao` VARCHAR(255) NULL DEFAULT NULL,
-  `turma_codigo` INT(11) NOT NULL,
-  PRIMARY KEY (`codigo`, `turma_codigo`),
-  CONSTRAINT `fk_disciplina_turma1`
-    FOREIGN KEY (`turma_codigo`)
-    REFERENCES `faculdade`.`turma` (`codigo`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
-
 DROP TABLE IF EXISTS `faculdade`.`professor` ;
 CREATE TABLE IF NOT EXISTS `faculdade`.`professor` (
   `codigo` INT(11) NOT NULL AUTO_INCREMENT,
   `nome` VARCHAR(45) NOT NULL,
   `cpf` VARCHAR(45) NOT NULL,
   `data_nasc` DATE NOT NULL,
-  `disciplina_codigo` INT(11) NOT NULL,
-  PRIMARY KEY (`codigo`, `disciplina_codigo`),
-  CONSTRAINT `fk_professor_disciplina1`
-    FOREIGN KEY (`disciplina_codigo`)
-    REFERENCES `faculdade`.`disciplina` (`codigo`)
+  PRIMARY KEY (`codigo`));
+
+DROP TABLE IF EXISTS `faculdade`.`disciplina` ;
+CREATE TABLE IF NOT EXISTS `faculdade`.`disciplina` (
+  `codigo` INT(11) NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(45) NOT NULL,
+  `descricao` VARCHAR(255) NULL DEFAULT NULL,
+  `professor_codigo` INT(11) NOT NULL,
+  `turma_codigo` INT(11) NOT NULL,
+  PRIMARY KEY (`codigo`),
+  CONSTRAINT `fk_disciplina_professor`
+    FOREIGN KEY (`professor_codigo`)
+    REFERENCES `faculdade`.`professor` (`codigo`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_disciplina_turma`
+    FOREIGN KEY (`turma_codigo`)
+    REFERENCES `faculdade`.`turma` (`codigo`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
 
 INSERT INTO `faculdade`.`usuario`(login, senha) VALUES ("administrador", "123456");
