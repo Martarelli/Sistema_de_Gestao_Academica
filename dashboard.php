e82f47a38b32214185c9a303f7604b9860ad014e
Martarelli <renanmartarelli@gmail.com>
2023-06-04 20:56:52 -0300

    dashboard

--- dashboard.php
+++ dashboard.php
@@ -15,8 +15,9 @@ require_once("sidemenu.php");
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 <body class="w-100 d-flex m-5">
-	<div class="container d-flex p-5 justify-content-center">
-		<h1>Bem vindo ao Sistema de Gestão Academica</h1>
+	<div class="container d-flex flex-column p-5 align-items-center">
+		<h1 class="mb-5">Bem vindo ao Sistema de Gestão Academica</h1>
+		<img src="dashboard-img.svg" width="500"alt="">
 		<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 		<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.1/dist/umd/popper.min.js"></script>
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
