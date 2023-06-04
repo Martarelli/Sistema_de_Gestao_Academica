fa48e2f1f8214f1a641d80a3e26c396f09c0185a
Martarelli <renanmartarelli@gmail.com>
2023-06-04 16:49:04 -0300

    aplicando vh 100 no side menu

--- sidemenu.php
+++ sidemenu.php
@@ -5,7 +5,7 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 	<link rel="stylesheet" href="style.css">
 </head>
-<body class="w-100 d-flex">
+<body class="w-100 d-flex vh-100">
 <div class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style="width: 280px;">
     <a href="/Sistema_de_Gestao_Academica/dashboard.php" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
       <i class="fa-solid fa-book fa-bounce"></i>
