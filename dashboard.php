6cdca21a3af1ff15df3f31f00142107cc9a2053d
Martarelli <renanmartarelli@gmail.com>
2023-06-04 20:36:52 -0300

    atualizado o dashboard

--- dashboard.php
+++ dashboard.php
@@ -15,7 +15,7 @@ require_once("sidemenu.php");
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 <body class="w-100 d-flex m-5">
-	<div class="container d-flex m-5 just">
+	<div class="container d-flex p-5 justify-content-center">
 		<h1>Bem vindo ao Sistema de Gestão Academica</h1>
 		<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 		<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.1/dist/umd/popper.min.js"></script>
