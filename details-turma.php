30c4eea5c4d7d7b5c2d47da4ff69a13346223513
Martarelli <renanmartarelli@gmail.com>
2023-06-13 12:02:55 -0300

    detalhes de registro turmas

--- details-turma.php
+++ details-turma.php
@@ -8,9 +8,13 @@ if (!isset($_SESSION['loggedin'])){
 
 require("sidemenu.php");
 
+if(isset($_GET['codigo'])){
+  $codigo = $_GET['codigo'];
+}
+
 require_once('connection.php');
 
-$mysql_query = "SELECT * FROM turma ORDER BY codigo";
+$mysql_query = "SELECT * FROM turma WHERE codigo = '{$codigo}'";
 $result = $conn->query($mysql_query);
 
 mysqli_close($conn);
@@ -19,14 +23,13 @@ mysqli_close($conn);
   <h2>Turmas</h2>
   <p>Listagem das turmas cadastradas.</p>
   <hr>
-  <div class="float-right p-1">
-    <a href="insert-turma.php"><button type="button" class="btn btn-dark mb-3">Adicionar Turma</button></a>
-  </div>
   <table class="table table-striped table-bordered table-hover">
     <thead>
       <tr class="table-dark" style="text-align:center">
         <th scope="col" style="width: 10%;">Código</th>
         <th scope="col">Curso</th>
+        <th scope="col" style="width: 15%;">Semestre</th>
+        <th scope="col" style="width: 15%;">Periodo</th>
         <th scope="col" style="width: 20%;">Ação</th>
       </tr>
     </thead>
@@ -35,12 +38,19 @@ mysqli_close($conn);
       <tr> 
         <th scope="row" style="text-align:center"><?php echo $data['codigo']; ?></th>
         <td style="text-align:center"><?php echo $data['curso']; ?></td> 
+        <td style="text-align:center"><?php echo $data['semestre']; ?></td> 
+        <td style="text-align:center"><?php echo $data['periodo']; ?></td> 
         <td style="text-align:center">
-          <a href="details-turma.php?codigo=<?php echo $data['codigo']; ?>">
-            <button type="button" class="btn btn-dark">Detalhes</button></a>
+          <a href="update-turma.php?codigo=<?php echo $data['codigo']; ?>">
+            <button type="button" class="btn btn-dark">Editar</button></a>
+          <a href="delete-turma.php?codigo=<?php echo $data['codigo']; ?>">
+            <button type="button" class="btn btn-danger">Excluir</button></a>
         </td> 
       </tr> 
       <?php } ?>       
     </tbody>
   </table>
+  <div class="float-right p-1">
+    <a href="turmas.php"><button type="button" class="btn btn-dark mb-3">Voltar</button></a>
+  </div>
 </div>
